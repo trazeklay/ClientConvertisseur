77dc47ffa00c9989894877c9c3d244f13a92e241
Clément TROMPIER <clem.trompier@gmail.com>
2023-01-30 15:33:49 +0100

    Création des tests unitaires pour le viewModel du ConvertisseurEuro

---
+++ ClientConvertisseurV2Tests/ViewModels/ConvertisseurEuroViewModelTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConvertisseurV2Tests.ViewModels
+{
+    internal class ConvertisseurEuroViewModelTests
+    {
+    }
+}
