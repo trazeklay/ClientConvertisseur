25f7dae835456487c4d5e9d3179f2423fec3de13
Clément TROMPIER <clem.trompier@gmail.com>
2023-02-01 15:13:11 +0100

    Avec code propre sur la gestion des pages

--- ClientConvertisseurV2/ViewModels/ConvertisseurEuroViewModel.cs
+++ ClientConvertisseurV2/ViewModels/ConvertisseurEuroViewModel.cs
@@ -15,100 +15,13 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace ClientConvertisseurV2.ViewModels
 {
-    public class ConvertisseurEuroViewModel: ObservableObject, INotifyPropertyChanged
+    public class ConvertisseurEuroViewModel : ConvertisseurViewModel
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-        private void OnPropertyChanged(string name)
+        public ConvertisseurEuroViewModel() : base() { }
+
+        protected override void CalculConversion()
         {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(name));
-            }
-        }
-
-        private ObservableCollection<Devise> lesDevises;
-        public ObservableCollection<Devise> LesDevises
-        {
-            get { return lesDevises; }
-            set
-            {
-                lesDevises = value;
-                OnPropertyChanged(nameof(LesDevises));
-            }
-        }
-
-        private double montantEuros;
-        public double MontantEuros
-        {
-            get { return montantEuros; }
-            set
-            {
-                montantEuros = value;
-                OnPropertyChanged(nameof(MontantEuros));
-            }
-        }
-
-        private Devise? devise;
-        public Devise? Devise
-        {
-            get { return devise; }
-            set
-            {
-                devise = value;
-                OnPropertyChanged(nameof(Devise));
-            }
-        }
-
-        private double montantDevise;
-        public double MontantDevise
-        {
-            get { return montantDevise; }
-            set
-            {
-                montantDevise = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public IRelayCommand BtnSetConversion { get; }
-
-        public ConvertisseurEuroViewModel()
-        {
-            this.GetDataOnLoadAsync();
-            BtnSetConversion = new RelayCommand(ActionSetConversion);
-        }
-
-        public void ActionSetConversion()
-        {
-            if (MontantDevise <= 0)
-                MessageBoxAsync("Le montant initial doit être supérieur à 0", "Error !");
-            else if (Devise is null)
-                MessageBoxAsync("Veuillez sélectionner une devise", "Error !");
-            else
-                MontantEuros = MontantDevise / Devise.Taux;
-        }
-
-        public async void GetDataOnLoadAsync()
-        {
-            WSService service = new WSService("https://loalhost:44394/api/");
-            List<Devise> result = await service.GetDevisesAsync("devises");
-            if (result == null)
-                MessageBoxAsync("API non disponible !", "Error !");
-            else
-                LesDevises = new ObservableCollection<Devise>(result);
-        }
-
-        private async void MessageBoxAsync(string content, string title)
-        {
-            ContentDialog dialog = new ContentDialog();
-            dialog.XamlRoot = App.MainRoot.XamlRoot;
-            dialog.Title = title;
-            dialog.Content = content;
-            dialog.CloseButtonText = "OK";
-
-
-            ContentDialogResult result = await dialog.ShowAsync();
+            MontantDevise = MontantEuros * Devise.Taux;
         }
     }
 }
---
+++ ClientConvertisseurV2/ViewModels/ConvertisseurViewModel.cs
@@ -0,0 +1,116 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClientConvertisseurV2.Models;
+using ClientConvertisseurV2.Services;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ClientConvertisseurV2.ViewModels
+{
+    public abstract class ConvertisseurViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        protected ObservableCollection<Devise> lesDevises;
+        public ObservableCollection<Devise> LesDevises
+        {
+            get { return lesDevises; }
+            set
+            {
+                lesDevises = value;
+                OnPropertyChanged(nameof(LesDevises));
+            }
+        }
+
+        protected double montantEuros;
+        public double MontantEuros
+        {
+            get { return montantEuros; }
+            set
+            {
+                montantEuros = value;
+                OnPropertyChanged(nameof(MontantEuros));
+            }
+        }
+
+        protected Devise? devise;
+        public Devise? Devise
+        {
+            get { return devise; }
+            set
+            {
+                devise = value;
+                OnPropertyChanged(nameof(Devise));
+            }
+        }
+
+        protected double montantDevise;
+        public double MontantDevise
+        {
+            get { return montantDevise; }
+            set
+            {
+                montantDevise = value;
+                OnPropertyChanged(nameof(MontantDevise));
+            }
+        }
+
+        public IRelayCommand BtnSetConversion { get; }
+
+        public ConvertisseurViewModel()
+        {
+            this.GetDataOnLoadAsync();
+            BtnSetConversion = new RelayCommand(ActionSetConversion);
+        }
+
+        public void ActionSetConversion()
+        {
+            if (MontantEuros <= 0)
+                MessageBoxAsync("Le montant initial doit être supérieur à 0", "Error !");
+            else if (Devise is null)
+                MessageBoxAsync("Veuillez sélectionner une devise", "Error !");
+            else
+                CalculConversion();
+        }
+
+        protected abstract void CalculConversion();
+
+        public async void GetDataOnLoadAsync()
+        {
+            WSService service = new WSService("https://localhost:44394/api/");
+            List<Devise> result = await service.GetDevisesAsync("devises");
+            if (result == null)
+                MessageBoxAsync("API non disponible !", "Error !");
+            else
+                LesDevises = new ObservableCollection<Devise>(result);
+        }
+
+        protected async void MessageBoxAsync(string content, string title)
+        {
+            ContentDialog dialog = new ContentDialog();
+            dialog.XamlRoot = App.MainRoot.XamlRoot;
+            dialog.Title = title;
+            dialog.Content = content;
+            dialog.CloseButtonText = "OK";
+
+
+            ContentDialogResult result = await dialog.ShowAsync();
+        }
+    }
+}
